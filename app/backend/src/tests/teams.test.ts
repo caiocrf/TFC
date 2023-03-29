d714ceef90101c5f18c2cca5582206bb6a5bce4b
caio pinho <icandoit2026@gmail.com>
2023-03-29 14:27:58 -0300

    error evaluator

---
+++ app/backend/src/tests/teams.test.ts
@@ -0,0 +1,18 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Seu teste', () => {
+
+
+});
